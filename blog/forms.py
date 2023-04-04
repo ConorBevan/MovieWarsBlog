36fdccc486d1c8ee051dcc474be5807ad70b4c84
ConorBevan <conor.bevan@hotmail.com>
2023-04-04 18:52:30 +0000

    tidied up editing page and view

--- blog/forms.py
+++ blog/forms.py
@@ -5,11 +5,11 @@ from .models import Post
 class CreatePost(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'author', 'content', 'featured_image')
+        fields = ('title', 'content', 'featured_image')
 
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
-            'author': forms.Select(attrs={'class': 'form-control'}),
+            # 'author': forms.Select(attrs={'class': 'form-control'}),
             'content': forms.Textarea(attrs={'class': 'form-control'}),
             'featured_image': forms.FileInput(attrs={'class': 'form-control'}),
         }
--- blog/views.py
+++ blog/views.py
@@ -12,27 +12,24 @@ from .forms import CreatePost
 # def about(request):
 #     return render(request, 'about.html')
 
-# Lists blogs on the home page
+
 class HomeView(ListView):
     model = Post
     template_name = 'home.html'
 
 
-# Shows specfic blog post
 class BlogDetailView(DetailView):
     model = Post
     template_name = 'blog_details.html'
 
 
-# Create new posts in the frontend
 class AddPostView(CreateView):
     model = Post
     form_class = CreatePost
     template_name = 'add_post.html'
 
 
-# Edit posts
 class EditView(UpdateView):
     model = Post
+    form_class = CreatePost
     template_name = 'update_post.html'
-    fields = ['title', 'content', 'featured_image']
