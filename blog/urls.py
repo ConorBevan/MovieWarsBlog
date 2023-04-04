71e1d232f08e4f2a4db7608923010039bda9c75d
ConorBevan <conor.bevan@hotmail.com>
2023-04-04 12:02:21 +0000

    created first view that the functionality is working but needs to be expanded in the front end display

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from . import views
+# from . import views
+from .views import HomeView
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('about/', views.about, name='blog-about'),
+    # path('', views.home, name='home'),
+    # path('about/', views.about, name='blog-about'),
+    path('', HomeView.as_view(), name="home"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from django.http import HttpResponse
+from .models import Post
 
 
-def home(request):
-    return render(request, 'home.html')
+# def home(request):
+#     return render(request, 'home.html')
 
 
-def about(request):
-    return render(request, 'about.html')
+# def about(request):
+#     return render(request, 'about.html')
+
+
+class HomeView(ListView):
+    model = Post
+    template_name = 'home.html'
