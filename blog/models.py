b464076e23bbf2a1e8025e34e2ec73bb21d62015
ConorBevan <conor.bevan@hotmail.com>
2023-04-04 11:34:47 +0000

    created post model and can create posts in admin site

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=300)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    featured_image = CloudinaryField('image', default='placeholder')
+
+    def __str__(self):
+        return self.title
