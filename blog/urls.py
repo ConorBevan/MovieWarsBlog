13e336d64dc751df1e6c876331f466c0e7161cd9
ConorBevan <conor.bevan@hotmail.com>
2023-04-04 17:15:50 +0000

    added a new page to create blog posts in the frontend

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 # from . import views
-from .views import HomeView, BlogDetailView
+from .views import HomeView, BlogDetailView, AddPostView
 
 urlpatterns = [
     # path('', views.home, name='home'),
-    # path('about/', views.about, name='blog-about'),
     path('', HomeView.as_view(), name="home"),
     path('Blogs/<int:pk>', BlogDetailView.as_view(), name="blog-detail"),
+    path('add_post/', AddPostView.as_view(), name='add-post'),
 ]
