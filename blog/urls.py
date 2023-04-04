e5f6ddf93bcf376130a13514dff7d4b01b916727
ConorBevan <conor.bevan@hotmail.com>
2023-04-04 12:45:08 +0000

    created second view for displaying the blog content when you click on the blog from the homepage

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 # from . import views
-from .views import HomeView
+from .views import HomeView, BlogDetailView
 
 urlpatterns = [
     # path('', views.home, name='home'),
     # path('about/', views.about, name='blog-about'),
     path('', HomeView.as_view(), name="home"),
+    path('Blogs/<int:pk>', BlogDetailView.as_view(), name="blog-detail"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -15,3 +15,8 @@ from .models import Post
 class HomeView(ListView):
     model = Post
     template_name = 'home.html'
+
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'blog_details.html'
