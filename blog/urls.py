75676ca21ebfff8b97f6f600086e0662df7c80a8
ConorBevan <conor.bevan@hotmail.com>
2023-04-04 18:06:21 +0000

    added edit button with functionality to my blog posts

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 # from . import views
-from .views import HomeView, BlogDetailView, AddPostView
+from .views import HomeView, BlogDetailView, AddPostView, EditView
 
 urlpatterns = [
     # path('', views.home, name='home'),
-    path('', HomeView.as_view(), name="home"),
-    path('Blogs/<int:pk>', BlogDetailView.as_view(), name="blog-detail"),
+    path('', HomeView.as_view(), name='home'),
+    path('Blogs/<int:pk>', BlogDetailView.as_view(), name='blog-detail'),
     path('add_post/', AddPostView.as_view(), name='add-post'),
+    path('Blogs/Edit/<int:pk>', EditView.as_view(), name='edit-blog'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from django.http import HttpResponse
 from .models import Post
 from .forms import CreatePost
@@ -12,18 +12,27 @@ from .forms import CreatePost
 # def about(request):
 #     return render(request, 'about.html')
 
-
+# Lists blogs on the home page
 class HomeView(ListView):
     model = Post
     template_name = 'home.html'
 
 
+# Shows specfic blog post
 class BlogDetailView(DetailView):
     model = Post
     template_name = 'blog_details.html'
 
 
+# Create new posts in the frontend
 class AddPostView(CreateView):
     model = Post
     form_class = CreatePost
     template_name = 'add_post.html'
+
+
+# Edit posts
+class EditView(UpdateView):
+    model = Post
+    template_name = 'update_post.html'
+    fields = ['title', 'content', 'featured_image']
