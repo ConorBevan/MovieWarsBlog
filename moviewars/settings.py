ffab8f0c502b2954b8b92e5eb3d4942c8020bae0
ConorBevan <conor.bevan@hotmail.com>
2023-04-17 22:54:42 +0000

    settings.py

--- moviewars/settings.py
+++ moviewars/settings.py
@@ -153,7 +153,7 @@ CLOUDINARY_STORAGE_AUTO_TAGGING = False
 
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
