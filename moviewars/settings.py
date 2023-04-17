779a8d9175d72054aabceb3559c14e5f69375f77
ConorBevan <conor.bevan@hotmail.com>
2023-04-17 11:13:49 +0000

    testing deployment

--- moviewars/settings.py
+++ moviewars/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['movie-wars-blog.herokuapp.com', 'localhost']
 
@@ -155,3 +155,5 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+X_FRAME_OPTIONS = SAMEORIGIN
