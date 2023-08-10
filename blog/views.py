61b5f0592bb281c05bf8e6ac56616a2e0a6b96dd
ConorBevan <conor.bevan@hotmail.com>
2023-08-10 11:55:01 +0000

    Added notification of successful or failed CRUD operations

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse
 from .models import Post
 from .forms import CreatePost
 from django.urls import reverse_lazy
+from django.contrib import messages
 
 
 class HomeView(ListView):
@@ -23,14 +24,29 @@ class AddPostView(CreateView):
     form_class = CreatePost
     template_name = 'add_post.html'
 
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        messages.success(self.request, 'Post created successfully!')
+        return response
+
 
 class EditView(UpdateView):
     model = Post
     form_class = CreatePost
     template_name = 'update_post.html'
 
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        messages.success(self.request, 'Post edited successfully!')
+        return response
+
 
 class DeletePost(DeleteView):
     model = Post
     template_name = 'delete_post.html'
     success_url = reverse_lazy('home')
+
+    def delete(self, request, *args, **kwargs):
+        response = super().delete(request, *args, **kwargs)
+        messages.success(self.request, 'Post deleted successfully!')
+        return response
